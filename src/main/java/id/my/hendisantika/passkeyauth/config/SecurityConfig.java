07d063d3aaced2598e79153da409e94baf46bd95
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 05:42:46 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/passkeyauth/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.passkeyauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-passkey-auth
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 05.42
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
