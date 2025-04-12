0b04065b84dc6e1569c6a30d71a8b09861ba3b00
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 05:41:14 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/passkeyauth/SpringBootPasskeyAuthApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.passkeyauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPasskeyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPasskeyAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/passkeyauth/SpringBootPasskeyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.passkeyauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPasskeyAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
