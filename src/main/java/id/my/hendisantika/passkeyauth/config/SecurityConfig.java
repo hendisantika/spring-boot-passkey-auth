be1946edb2b8b81b69d58444a53e92f95c46b757
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 05:43:29 +0700

    feat: Add userDetailsService in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/passkeyauth/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/passkeyauth/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package id.my.hendisantika.passkeyauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +27,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("yuji")
+                .password(passwordEncoder()
+                        .encode("1234"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
