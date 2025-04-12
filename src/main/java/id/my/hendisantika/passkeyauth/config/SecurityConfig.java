750aef07b250f9443e9806f72e8732520652b3b0
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 05:43:52 +0700

    feat: Add securityFilterChain in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/passkeyauth/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/passkeyauth/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package id.my.hendisantika.passkeyauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by IntelliJ IDEA.
@@ -39,5 +42,21 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        req ->
+                                req.anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults())
+                .webAuthn(
+                        webauthn -> webauthn
+                                .rpName("Spring Security Relying Party")
+                                .rpId("localhost")
+                                .allowedOrigins("http://localhost:8080")
+                );
+        return http.build();
     }
 }
